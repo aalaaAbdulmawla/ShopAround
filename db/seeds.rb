dc432d801493c3e3326eef91e50a4dba12827428
Aalaa Mohamed <alaa.abdulmawla1993@gmail.com>
2015-01-26 03:12:57 +0200

    Creating The Project Files

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+$i = 1
+$num = 5
+
+until $i > $num  do
+	Product.create!(title:"Item ##{$i}",
+					description:
+						%{<p>
+							Description Of Item ##{$i} 
+						</p>},
+					image_url:'item.jpg',
+					price: "#{$i*10}" )
+	$i += 1;
+end 
