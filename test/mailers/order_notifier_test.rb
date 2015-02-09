e646c703e5eae11c07f2f7a209a87570b0bb8920
Aalaa Mohamed <alaa.abdulmawla1993@gmail.com>
2015-02-10 01:52:05 +0200

    Finish OrderNotifier test

---
+++ test/mailers/order_notifier_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class OrderNotifierTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Shop Around Order Confirmation", mail.subject
+    assert_equal ["alaa.abdulmawla@gmail.com"], mail.to
+    assert_equal ["aalaa.abdelmawla@gmail.com"], mail.from
+    assert_match /1 x MyString/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Shop Around Order Shipped", mail.subject
+    assert_equal ["alaa.abdulmawla@gmail.com"], mail.to
+    assert_equal ["aalaa.abdelmawla@gmail.com"], mail.from
+    assert_match /<td>&times;1<\/td>/, mail.body.encoded
+  end
+
+end
